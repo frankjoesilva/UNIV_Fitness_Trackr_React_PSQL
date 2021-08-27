6edbdd784304edb296e7315bbc6cda33534b468d
Frank Silva <frankjoesilva@gmail.com>
2021-08-26 17:56:35 -0700

    working on count and duration api routes

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -33,27 +33,27 @@ const MyRoutines = ({
     useEffect(() => {
         if (userToken) {
             getRoutinesByUsername(user.username, userToken)
-                .then(routines => {
-                    console.log(routines)
-                    setMyRoutines(routines)
+                .then(myRoutines => {
+                    console.log(myRoutines)
+                    setMyRoutines(myRoutines)
                 })
                 .catch(error => {
                     console.error(error)
                 });
         }
 
-    }, [userToken, user]);
+    }, [userToken, user.username]);
 
     useEffect(() => {
         getActivities()
             .then(activities => {
-                // console.log(activities, "xx")
+                console.log(activities, "xx")
                 setActivities(activities)
             })
             .catch(error => {
                 console.error(error)
             });
-    }, [userToken]);
+    }, [userToken, user]);
 
 
 
@@ -70,14 +70,16 @@ const MyRoutines = ({
                 try {
                     const addCountAndDuration = await postActivityCountAndDuration(activityCount, activityDuration, userToken, user.id)
                     const addRoutine = await postRoutines(routineName, routineGoal, isPublic, userToken, user.id)
-                    if (routineName === '' || routineGoal === '') {
+                    if (routineName === '' || routineGoal === '' || activityCount === '' || activityDuration === '') {
                         setError('Missing Fields')
                     } else {
-                        setMyRoutines([...myRoutines, addRoutine, addCountAndDuration])
+                        setMyRoutines([...myRoutines, addRoutine])
+                            &&
+                            setActivities([...activities, addCountAndDuration])
 
                     }
                 } catch (error) {
-                    setError(error)
+                    console.error(error)
                 }
             }}
             className='my-routines'>
@@ -89,9 +91,10 @@ const MyRoutines = ({
                     <Form.Group controlId="routineName">
 
                         <Form.Control value={routineName} type="text" placeholder="Routine Name" onChange={(event) => {
-                            const routine = event.target.value
-                            setRoutineName(routine)
+                            const routineName = event.target.value
+                            setRoutineName(routineName)
                         }} />
+
                         {error ? <div>{error}</div> : null}
 
                     </Form.Group>
@@ -146,6 +149,15 @@ const MyRoutines = ({
 
                     </Form.Group>
 
+                    <Form.Group controlId="activityDuration">
+
+                        <Form.Control value={activityDuration} type="string" placeholder="Duration" onChange={(event) => {
+                            const duration = event.target.value
+                            setActivityDuration(duration)
+                        }} />
+
+                    </Form.Group>
+
 
                     <Form.Check value={isPublic} id='checkbox' className='m-auto' style={{ color: 'limegreen' }} type="checkbox" label="Public" onChange={() => {
 
@@ -165,59 +177,50 @@ const MyRoutines = ({
                             // console.log(routine)
 
                             return (
-                                <CardDeck
-                                    key={routine.id}>
+                                <CardDeck key={routine.id}>
                                     <Container>
+                                        <Accordion>
 
-                                        <Card id="myRoutines-card"
-                                            className="focus mt-2 mb-2"
-                                        >
-                                            <Card.Body id='card-container'>
-                                                <Card.Header className="text-center  card-title">My Routines </Card.Header>
-                                                <Card.Text className="ml-auto">Routine Name: {routine.name}</Card.Text>
-                                                <Card.Text className="ml-auto">Routine Goal: {routine.goal}</Card.Text>
-                                                {/* <Card.Header className="text-center  card-title">Activities For Routines </Card.Header> */}
+                                            <Card id="myRoutines-card"
+                                                className="focus mt-2 mb-2"
+                                            >
+                                                <Card.Body id='card-container'>
+                                                    <Card.Header className="text-center  card-header">My Routines </Card.Header>
+                                                    <Card.Text className="ml-auto">Routine Name: {routine.name}</Card.Text>
+                                                    <Card.Text className="ml-auto">Routine Goal: {routine.goal}</Card.Text>
 
-                                                {/* <h3 id='activities-title'>Activities For Routines</h3> */}
-                                                {/* {routine.activities && routine.activities.map((activity, index) => {
-                                                    return (
-                                                        <React.Fragment key={index}>
-                                                            <Card.Title className="ml-auto">Activity Name: {activity.name}</Card.Title>
-                                                            <Card.Text className="ml-auto">Activity Description{activity.description}</Card.Text>
-                                                            <Card.Text className="ml-auto">Activity Duration: {activity.duration}</Card.Text>
-                                                            <Card.Text className="ml-auto">Activity Count: {activity.count}</Card.Text>
-                                                        </React.Fragment> */}
-                                                <Accordion.Toggle
-                                                    id='toggle'
-                                                    as={Card.Header}
-                                                    eventKey="0"
-                                                    className="p-2 text-center accordian-main"
-                                                >
-                                                    ACTIVITY DETAILS
-                                                </Accordion.Toggle>
-                                                {routine.activities && routine.activities.map((activity, index) => {
-                                                    return (
+                                                    <Accordion.Toggle
+                                                        id='toggle'
+                                                        as={Card.Header}
+                                                        eventKey="0"
+                                                        className="p-2 text-center accordian-main"
+                                                    >
+                                                        ACTIVITY DETAILS
+                                                    </Accordion.Toggle>
+                                                    {routine.activities && routine.activities.map((activity, index) => {
 
-                                                        <Accordion.Collapse eventKey="0" className="text-left" key={index}>
-                                                            <React.Fragment>
-                                                                <Card.Text className="ml-auto">Activity Name: {activity.name}</Card.Text>
-                                                                <Card.Text className="ml-auto">Activity Description: {activity.description}</Card.Text>
-                                                                <Card.Text className="ml-auto">Activity Duration: {activity.duration}</Card.Text>
-                                                                <Card.Text className="ml-auto">Activity Count: {activity.count}</Card.Text>
-                                                            </React.Fragment>
-                                                        </Accordion.Collapse>
-                                                    )
-                                                })}
-                                                <div id='edit-delete-btn'>
-                                                    <Button id='delete-btn' style={{ width: '5.5rem', background: 'red' }} variant="primary" onClick={async () => { }}>
-                                                        DELETE
-                                                    </Button>
-                                                    <Button id='edit-btn' style={{ width: '5.5rem', background: 'red' }} variant="primary" onClick={async () => { }}>
-                                                        EDIT
-                                                    </Button>
-                                                </div>
-                                            </Card.Body>
-                                        </Card>
+                                                        return (
+                                                            <Accordion.Collapse eventKey="0" className="text-left" key={index}>
+                                                                <React.Fragment>
+                                                                    <Card.Text className="ml-auto">Activity Name: {activity.name}</Card.Text>
+                                                                    <Card.Text className="ml-auto">Activity Description: {activity.description}</Card.Text>
+                                                                    <Card.Text className="ml-auto">Activity Duration: {activity.duration}</Card.Text>
+                                                                    <Card.Text className="ml-auto">Activity Count: {activity.count}</Card.Text>
+                                                                </React.Fragment>
+                                                            </Accordion.Collapse>
+                                                        )
+                                                    })}
+                                                    <div id='edit-delete-btn'>
+                                                        <Button id='delete-btn' style={{ width: '5.5rem', background: 'red' }} variant="primary" onClick={async () => { }}>
+                                                            DELETE
+                                                        </Button>
+                                                        <Button id='edit-btn' style={{ width: '5.5rem', background: 'red' }} variant="primary" onClick={async () => { }}>
+                                                            EDIT
+                                                        </Button>
+                                                    </div>
+                                                </Card.Body>
+                                            </Card>
+                                        </Accordion>
                                     </Container>
                                 </CardDeck>
                             )
@@ -226,8 +229,7 @@ const MyRoutines = ({
                             return null
                         }
 
-                    })
-                }
+                    })}
             </div>
         </Form >
     );
